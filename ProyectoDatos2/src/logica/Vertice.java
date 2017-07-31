d4944ba667172966607132e9039f02fa18b0a0be
DESKTOP-0H4UH09\Cherng <czhangc@ucenfotec.ac.cr>
2017-07-30 22:11:18 -0600

    Se agrego el nodo del vertice

--- ProyectoDatos2/src/logica/Vertice.java
+++ ProyectoDatos2/src/logica/Vertice.java
@@ -5,10 +5,64 @@
  */
 package logica;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jgm16
  */
 public class Vertice {
+    private int numeroVertice;
+    private String etiqueta;
+    private ArrayList<Arco> predecesor;
+    private ArrayList<Arco>  sucesor;
     
+    public Vertice(int pnumVertice, String petiqueta){
+        this.setEtiqueta(petiqueta);
+        this.setNumeroVertice(pnumVertice);
+        predecesor=null;
+        sucesor=null;
+    }
+    public int getNumeroVertice() {
+        return numeroVertice;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public ArrayList<Arco> getPredecesor() {
+        return predecesor;
+    }
+
+    public ArrayList<Arco> getSucesor() {
+        return sucesor;
+    }
+
+    public void setNumeroVertice(int numeroVertice) {
+        this.numeroVertice = numeroVertice;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public void setPredecesor(Arco ppredecesor) {
+        if(predecesor == null){
+            this.predecesor = new ArrayList<Arco>();
+            this.predecesor.add(ppredecesor);
+        }else{
+            this.predecesor.add(ppredecesor);
+        }
+    }
+
+    public void setSucesor(Arco psucesor) {
+        
+        if(sucesor == null){
+            this.sucesor = new ArrayList<Arco>();
+            this.sucesor.add(psucesor);
+        }else{
+            this.sucesor.add(psucesor);
+        }
+    }
 }
